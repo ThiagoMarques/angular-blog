fcd83dd780efcb3c8267712b450b2a5245db8c16
Thiago Marques <thiagossmarques@gmail.com>
2023-10-07 15:13:51 -0300

    feat: adiciona small cards

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -9,5 +9,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./small-card.component.scss']
 })
 export class SmallCardComponent {
-
+  imagePath = 'assets/img/example.png';
+  title = 'Saiu uma nova versão do Angular'
+  content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuTitleComponent } from 'src/app/components/menu-title/menu-title.component';
 import { BigCardComponent } from 'src/app/components/big-card/big-card.component';
+import { SmallCardComponent } from 'src/app/components/small-card/small-card.component';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,8 @@ import { BigCardComponent } from 'src/app/components/big-card/big-card.component
   imports: [
     CommonModule, 
     MenuTitleComponent,
-    BigCardComponent
+    BigCardComponent,
+    SmallCardComponent
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
